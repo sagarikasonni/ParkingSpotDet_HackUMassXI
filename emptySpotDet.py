025ee08b4dfa035a1b27500efa48fe1e664b649f
sagarikasonni <102849634+sagarikasonni@users.noreply.github.com>
2023-11-12 03:48:26 +0530

    Update emptySpotDet.py

--- emptySpotDet.py
+++ emptySpotDet.py
@@ -1,38 +1,76 @@
-# code to detect the empty spots using OpenCV
-import cv2 
+import cv2
 import numpy as np
-# starts the class containing the main working
-class SpotDet:
-    # constructor 
-    def __init__(self):
-        self.cnt = 0
-    # algorithm 
-    def isEmpty(self, path):
-        # reading the img returned by aws 
-        img = cv2.imread(path)
-        # convert image to grayscale
-        grayImg = cv2.cvtColor(img, cv2.COLOR_BAYER_BG2BGR)
-        # now I have to reduce noise and refine contour detection
-        # this can be done using gaussian blur
-        blurredImg = cv2.GaussianBlur(grayImg, (5, 5), 0)
-        # assuming parking spots are white -> need to define proper lower and upper bounds 
-        lowerBound = np.array([200, 200, 200], dtype=np.uint8)
-        upperBound = np.array([255, 255, 255], dtype=np.uint8)
-        # now create a mask for all values in this lower and upper bound range 
-        mask = cv2.inRange(img, lowerBound, upperBound)
-        # inorder to differentiate empty and free parking spots -> need to find contours in the masked map
-        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        # increment the counter with the length of each counter
-        self.cnt += len(contours)
-        # redraw contours 
-        cv2.drawContours(img, contours, -1, (0, 255, 0), 2)
-        # final display
-        cv2.imshow('The image is', img)
-        # will wait indefinitely until a key is pressed since no duration is mentioned 
-        cv2.waitKey(0)
-        # finally close all windows
-        cv2.destroyAllWindows()
-    # finally return the counter
-    def retCount(self):
-        return self.cnt
-    # TODO: adding the result from raspberry pi to image path, basically something like a main function
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+
+# Function to classify parking spots
+def classify_parking_spots(features, labels):
+    # Check if the number of features and labels is the same
+    if len(features) != len(labels):
+        raise ValueError("Number of features and labels must be the same.")
+
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+
+    # Feature scaling
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(np.array(X_train).reshape(-1, 1))
+    X_test_scaled = scaler.transform(np.array(X_test).reshape(-1, 1))
+
+    # Train a simple SVM classifier
+    svm_classifier = SVC(kernel='linear')
+    svm_classifier.fit(X_train_scaled, y_train)
+
+    # Predict on the test set
+    y_pred = svm_classifier.predict(X_test_scaled)
+
+    # Evaluate the model
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f"Model Accuracy: {accuracy}")
+
+    return svm_classifier
+
+# User input for image path
+image_path = input("Enter the path to the image: ")
+
+# Read the input image
+image = cv2.imread(image_path)
+
+# Convert the image to grayscale
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Apply Gaussian blur
+blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
+
+# Use edge detection techniques
+edges = cv2.Canny(blurred_image, 50, 150)
+
+# Find contours in the image
+contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Filter out small or irrelevant contours
+min_contour_area = 100
+filtered_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_contour_area]
+
+# Dynamic feature extraction
+features = [cv2.contourArea(cnt) for cnt in filtered_contours]
+
+# Example labels: 1 for occupied, 0 for free (label your data accordingly)
+labels = [1 if input(f"Is parking spot {i} occupied? (1 for yes, 0 for no): ") == '1' else 0 for i in range(len(features))]
+
+# Classify parking spots
+classifier = classify_parking_spots(features, labels)
+
+# Visualize the results
+for i, cnt in enumerate(filtered_contours):
+    x, y, w, h = cv2.boundingRect(cnt)
+    status = "Occupied" if classifier.predict(np.array([cv2.contourArea(cnt)]).reshape(1, -1)) == 1 else "Free"
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cv2.putText(image, status, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+# Display the results
+cv2.imshow('Parking Spots', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
