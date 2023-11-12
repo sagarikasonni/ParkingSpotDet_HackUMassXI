cf215b3ae00c5522a60a33da411222eb7654c6b6
sagarikasonni <102849634+sagarikasonni@users.noreply.github.com>
2023-11-12 09:12:03 +0530

    With means to load more images

--- trainingScript.py
+++ trainingScript.py
@@ -31,32 +31,43 @@ def extract_features_from_image(image):
 dataset_folder = 'pics'
 
 # Load a single image from the dataset
-filename = os.listdir(dataset_folder)[0]
-image_path = os.path.join(dataset_folder, filename)
-img = cv2.imread(image_path)
+# filename = os.listdir(dataset_folder)[0]
+# image_path = os.path.join(dataset_folder, filename)
+# img = cv2.imread(image_path)
 
 # Check if the image is loaded successfully
-if img is not None:
-    # Print the loaded image for debugging
-    print("Loaded image:", image_path)
 
-    # Extract features from the image
-    all_features = extract_features_from_image(img)
+# get the list of images 
+filename = os.listdir(dataset_folder)
+# iteratre thru the list for every image 
+for img_name in filename:
+    # get the image path 
+    image_path = os.path.join(dataset_folder, img_name)
+    # read the image 
+    img = cv2.imread(image_path)
 
-    # Convert the feature list to a numpy array
-    features_array = np.array(all_features).reshape(-1, 1)
+    if img is not None:
+        # Print the loaded image for debugging
+        print("Loaded image:", image_path)
 
-    # Use k-means clustering to identify parking spots
-    n_clusters = 2  # You may adjust this based on the number of parking spot classes you expect
-    n_init_value = 10  # Explicitly set the value for n_init
-    kmeans = KMeans(n_clusters=n_clusters, n_init=n_init_value, random_state=42)
-    kmeans.fit(features_array)
+        # Extract features from the image
+        all_features = extract_features_from_image(img)
 
-    # Assign labels to the clusters
-    labels = kmeans.labels_
+        # Convert the feature list to a numpy array
+        features_array = np.array(all_features).reshape(-1, 1)
 
-    # Save the trained k-means model
-    joblib.dump(kmeans, 'parking_spot_kmeans.pkl')
+        # Use k-means clustering to identify parking spots
+        n_clusters = 2  # You may adjust this based on the number of parking spot classes you expect
+        n_init_value = 10  # Explicitly set the value for n_init
+        kmeans = KMeans(n_clusters=n_clusters, n_init=n_init_value, random_state=42)
+        kmeans.fit(features_array)
 
-else:
-    print(f"Error loading image: {image_path}")
+        # Assign labels to the clusters
+        labels = kmeans.labels_
+
+        # Save the trained k-means model
+
+    else:
+        print(f"Error loading image: {image_path}")
+
+joblib.dump(kmeans, 'parking_spot_kmeans.pkl')
